aeb5aa88d839d4995de5802951fdcc97425b2b14
Basnyat27 <rockeyman2711@gmail.com>
2025-09-17 22:11:13 +0545

    add assignment1

---
+++ country/nepal.py
@@ -0,0 +1,5 @@
+def butwal():
+    print("I am from butwal")
+
+def ktm():
+    print("I am from ktm")
---
+++ country/usa.py
@@ -0,0 +1,7 @@
+def california():
+    print("I am from california")
+    
+
+def newyork():
+    # pass
+    return "I am from newyork"
